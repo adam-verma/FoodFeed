f011fbccd3d25ae6fe3f15f2acf1ca9012c1ae26
Kitrick Bell <kitrickd.bell@gmail.com>
2020-03-05 18:06:56 -0600

    styled the dummy nav bar

--- client/src/components/NavBar-Start/index.js
+++ client/src/components/NavBar-Start/index.js
@@ -5,7 +5,7 @@ import React from "react";
 // That way we don't have to define them all individually
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <a className="navbar-brand">FoodFeed</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
