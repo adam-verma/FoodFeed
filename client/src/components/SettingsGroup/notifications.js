116fe416ced0ff32cdf749f5ebf81f1f5ad42e8c
danman15726 <danman15726@gmail.com>
2020-03-09 01:37:00 -0500

    Notifications are done for now. CSS AND ACTIVE-CLASS-CSS-STATUS STILL NEED TO BE APPLIED. <TOGGLE/> AND <NOTIFICATIONS/> ARE USED FOR THIS. To switch components, I am using a useState and a componentMap. Very simple and straightforward. LOGIC SUBJECT TO CHANGE AFTER MODELS ARE FULLY IMPLEMENTED.

--- client/src/components/SettingsGroup/notifications.js
+++ client/src/components/SettingsGroup/notifications.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Notifications = (props) =>{
+const Notifications = (props) =>(
 // TODO:MAKE A DROP-DOWN MENU FOR EVERY NOTIFICATION; BY EMAIL OR BY PHONE.
 // GIVE THEM THE CHOICE OF WHICH STREAMER THEY WANT NOTIFICATIONS FROM. 
 // HAVE AN ALL-SELECT TOGGLE BUTTON
@@ -9,20 +9,17 @@ const Notifications = (props) =>{
 //IF NO EMAIL OR PHONE ON FILE FOR SOME REASON, HAVE AN '!'-RED-ALERT- 
 //-ICON RIGHT NEXT TO ACCOUNT SETTING
 
+        <div>
+            
+            <div>
+                <p1>{props.description1}</p1><br></br>
+                
+            </div>
 
-
-    return(
-        <>
-            <h1>Notifications</h1>
-            <p1>Receive an Email notification whenever your followed Chefs' stream </p1><br></br>
-            <p2>Receive a Mobile notification whenever your followed Chefs' stream</p2>
-        </>
+        </div>
 
     )
 
-
-
-}
 
 
 
--- client/src/pages/settings.js
+++ client/src/pages/settings.js
@@ -7,7 +7,7 @@ import Notifications from "../components/SettingsGroup/notifications.js";
 import Securiprivacy from "../components/SettingsGroup/securiprivacy.js";
 import Geolocation from "../components/SettingsGroup/geolocation.js";
 import Account from "../components/SettingsGroup/account.js";
-import Section from "../components/SettingsGroup/section.js";
+import Toggle from "../components/SettingsGroup/toggle.js";
 
 
 // TODO:This is not a real import. This helps me follow a train of thought. REMOVE AFTER DONE.
@@ -16,8 +16,53 @@ import MongooseData from "mongoose";
 
 const Settings = (props) => {
     const [component, setComponent] = useState("Notifications");
+    const [results, setResults] = useState([]);
+
+    const exampleChefs = ["Murphy","21 Savage", "23 Savage", "Joe Mama"];
+
+
+
     const componentMap = {
-        Notifications: <Notifications/>,
+        Notifications:
+        <>
+            <h1>Notifications</h1>
+            <Notifications
+                description1 = {"Notify Via Email"}
+                
+            />
+            <br></br>
+            <Toggle
+                name = {" All "}
+            />
+            {exampleChefs.map((result, i) =>(
+                
+                <Toggle
+                    name = {result}
+                />
+                
+            ))}
+            <br></br>
+            <br></br>
+
+
+            <Notifications
+                description1 = {"Notify Via SMS Text Message"}
+            />
+            <br></br>
+            <Toggle
+                name = {" All "}
+            />
+            {exampleChefs.map((result, i) =>(
+                
+                <Toggle
+                    name = {result}
+                />
+            ))}
+        
+        </>
+        
+        
+        ,
         Securiprivacy: <Securiprivacy/>,
         Geolocation: <Geolocation/>,
         Account: <Account/>
@@ -38,7 +83,7 @@ const Settings = (props) => {
             <aside>
                 <h1>Settings</h1>
                 <ul>
-                    <li tabIndex="1" className = "nav-item">
+                    <li tabIndex="1" className = "nav-item active">
                         <a className = "nav-link" type = "button" onClick={() => setComponent("Notifications")}>Notifications</a>
                     </li>
                     <li  tabIndex="1"className = "nav-item" >
