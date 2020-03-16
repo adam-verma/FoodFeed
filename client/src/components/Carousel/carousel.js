a1df7223e1496ee43c3cd703d02db97084253673
danman15726 <danman15726@gmail.com>
2020-03-16 00:47:44 -0500

    FINALLY FINISHED THE START PAGE MINUS SOME POLISH NEEDED. FOR FUTURE REFERENCE, WHEN UPDATING A VIDEO SRC, YOU MUST ALSO UPDATE THE VIDEO TAG SURROUNDING IT. ACCORDING TO REACT.JS, JUST BECAUSE YOU UPDATE THE SRC ATTRIBUTE, IT DOESN'T DEEM IT WORTHY TO UPDATE EVERYTHING, BECAUSE AS FAR AS IT'S CONCERNED, IT'S STILL THE VIDEO...ORSO THAT'S HOW IT THINKS. IT DOESN'T KNOW THAT IT IS INDEED A DIFFERENT VIDEO NEEDING TO BE RENDERED. THIS IS WHY THE KEY = {PROP.WHATEVER} WORKED. THE REACT DOM THEN KNOWS THAT OH, THIS IS INDEED CLEARLY A DIFFERENT VIDEO TRYING TO BE RENDERED. WITH EVERY PROP UPDATE, REACT NEEDS A LITTLE ASSURANCE/PUSH TO ENSURE THAT IT IS A DIFFERENT ELEMENT.

--- client/src/components/Carousel/carousel.js
+++ client/src/components/Carousel/carousel.js
@@ -1,22 +1,22 @@
 import React from "react";
+import "../../pages/styles/start/start.css";
 
 
 
-const Carousel = (props) =>(
-    
-    <div className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-            <video loop autoPlay className="carousel-item active">
-                <source className="d-block" src={props.vid1} alt="First"></source>
-            </video>
-            <video loop autoPlay className="carousel-item">
-                <source className="d-block" src={props.vid2} alt="Second"></source>
-            </video>
-            <video loop autoPlay className="carousel-item">
-                <source className="d-block" src={props.vid3} alt="Third"></source>
-            </video>
+const Pad = (props) =>(
+    <div className = "contain">
+        <div className = "Paragraph-opening">
+            <p>{props.text}</p>
+
         </div>
+        
+        <div className = "video">
+                <video loop autoPlay>
+                    <source className="d-block" src={props.video} alt="First"></source>
+                </video>
+        </div>
+
     </div>
     
 )
-export default Carousel;
+export default Pad;
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -1,57 +1,77 @@
-import React     from "react";
+import React, { useState, useEffect}     from "react";
+import ReactDOM from 'react-dom';
 
-import "../pages/styles/start/start.css";
-
-import Carousel from "../components/Carousel/carousel.js";
-import c1 from "../media/video/c1.mp4";
-import c2 from "../media/video/c2.mp4";
-import c3 from "../media/video/c3.mp4";
-import f1 from "../media/video/f1.mp4";
-import f2 from "../media/video/f2.mp4";
-import f3 from "../media/video/f3.mp4";
-import s1 from "../media/video/s1.mp4";
-import s2 from "../media/video/s2.mp4";
-import s3 from "../media/video/s3.mp4";
-
-
+import Pad from "../components/Pad/Pad.js";
+import cfs1 from "../media/video/cfs1.mp4";
+import cfs2 from "../media/video/cfs2.mp4";
+import cfs3 from "../media/video/cfs3.mp4";
+import { set } from "mongoose";
 
 
 const Start = () =>{
+    const [slide, setSlide] = useState("slide1");
+    const [video, setVideo] = useState("video1");
+
+    const videoMap ={
+        video1: cfs1,
+        video2: cfs2,
+        video3: cfs3
+    }
+
+    
+
+    const update = (slide) => {
+
+        setSlide(slide);
+        
+        document.createElement("source").src = cfs2;
+        
+    }
 
 
-
-return (
-        <div className = "contain">
-
-            <div className = "Paragraph-opening">
-                <p>
-                    A platform where world-class chefs create world-class dishes, and stream it to you at
-                    the comfort of your home.
-                </p>
-
-            </div>
-
-
-
-            <Carousel
-                vid1 = {c1 + "#t=1,4"}
-                vid2 = {f1 + "#t=1,4"}
-                vid3 = {s1 + "#t=1,4"}
-
-            
-            
+    
+    
+    const propMap = {
+        slide1:
+            <Pad
+                text = {"A platform where world-class chefs create world-class dishes, and you get the inside scoop into how it's done."}
+                video = {document.getElementsByTagName("source").src = cfs1}
+                click = {() => update("slide2")}
+                
+            />
+        ,
+        slide2:
+            <Pad
+                text = {"Learn new additions and techniques behind cooking your favorite dishes! Ask questions and join the community."}
+                video = {document.getElementsByTagName("source").src = cfs2}
+                click = {() => update("slide3")}
+            />
+        ,
+        slide3:
+            <Pad
+                text = {"Looks delish? Nearby? Order right from the comfort of your home!"}
+                video = {document.getElementsByTagName("source").src = cfs3}
+                click = {() => update("slide1")}
             />
 
 
+    }
 
 
-            
-            
-        </div>
+    
 
-    )
+    return (
+            <React.Fragment>
+
+                {propMap[slide]}
+                
+                
+                
+            </React.Fragment>
+
+        )
 }
 
 
 
-export default Start
+export default Start;
