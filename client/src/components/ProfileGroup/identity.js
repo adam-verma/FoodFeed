3a78ca45ceeb9dc7c4a78d3f942626a96e0a831f
danman15726 <danman15726@gmail.com>
2020-03-18 16:24:29 -0500

    Adding to the profile page. Making flex adjustments and accomodating for both mozilla and chrome. Target Audience. Created a list that has clickable text to potentially point to a preferred recipe. Created an about me page.Constantly making fitting adjustments based on margins, padding, flex properties, font size, borders and so on.

--- client/src/components/ProfileGroup/identity.js
+++ client/src/components/ProfileGroup/identity.js
@@ -8,29 +8,32 @@ const Identity = (prop) => (
 
         <div className = "div2 rounded">
             <div className = "profile">
-                <div className = "panel rounded">
+                <div className = "panel rounded border-bottom">
                     <div className = "panel-body">
                         
                         <img className = "img-thumbnail rounded" id = "image" src = {prop.image} key = {prop.image}></img>
                         {prop.name}
-                        <a href="#">
-                            <i class="fab fa-facebook-square border-right"></i>
-                        </a>
-                        <a href="#">
-                            <i class="fab fa-instagram-square border-right"></i>
-                        </a>
-                        <a href="#">
-                            <i class="fab fa-snapchat-square border-right"></i>
-                        </a>
-
 
                         
-                        
-                        <a href="#">
-                            <i class="fab fa-twitter-square"></i>
+                        <div className = "icons">
+                            <a href="#">
+                                <i class="fab fa-facebook-square border-right"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-instagram-square border-right"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-snapchat-square border-right"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter-square border-right"></i>
 
-                        </a>
-
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-patreon"></i>
+                            </a>
+                            <button className = "btn btn-primary btn-sm" id = "editProfile">Edit Profile</button>
+                        </div>
 
                     </div>
 
@@ -38,7 +41,62 @@ const Identity = (prop) => (
 
                 </div>
 
+
+
+                <div className = "container-1">
+                    <ul class="favoriteRecipes border" >
+                        <h2>Favorite Recipes </h2> 
+                        <li class="list-group-item">
+                            <a href={prop.line1link}>{prop.line1}</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href={prop.line2link}>{prop.line2}</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href={prop.line3link}>{prop.line3}</a>  
+                        </li>
+                        <li class="list-group-item">
+                            <a href={prop.line4link}>{prop.line4}</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href={prop.line5link}>{prop.line5}</a>
+                        </li>
+                    </ul>
+
+
+                    <div className = "panel2 rounded border" id = "aboutme">
+                        <div className = "panel-body"> 
+                         
+                        <h2>About Me</h2>
+                        <p>{prop.aboutMe}</p>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
+            
+
+
+
+
 
 
         </div>
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -30,6 +30,28 @@ const Dashboard = () => {
                 coverImage = {"https://via.placeholder.com/1000x200"}
                 image = {"https://via.placeholder.com/500"}
                 name = {"Dan Deva"}
+                aboutMe = {"Hey my name is Dan! Nice to meet you. I've been cooking for a bagillion years. I love to share the art to the rest of the world. While I am, inconsistent scheduling wise, I promise you wouldn't want to miss my streams!"}
+
+
+
+
+
+
+
+
+                line1 = {"Lasagna"}
+                line2 = {"Pizza"}
+                line3 = {"Clams Casino"}
+                line4 = {"Orange Chicken"}
+                line5 = {"Bacon-wrapped-macaroni-stuffed Peppers"}
+
+                // line1link = {}
+                // line2link = {}
+                // line3link = {}
+                // line4link = {}
+                // line5link = {}
+
+                
             />
         
 
