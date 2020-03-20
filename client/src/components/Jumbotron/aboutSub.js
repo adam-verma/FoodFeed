a752890c283d951d954f6866755d2fb02d79a55c
Kitrick Bell <kitrickd.bell@gmail.com>
2020-03-20 18:28:47 -0500

    styling the about page

--- client/src/components/Jumbotron/aboutSub.js
+++ client/src/components/Jumbotron/aboutSub.js
@@ -7,8 +7,12 @@ function JumbotronSub(props) {
   return (
     <div className="jumbotronSub jumbotron-fluid">
   <div >
-    <h1 className="display-4 col-md-12">About FoodFeed</h1>
-    <p className="lead"> Hungry?  </p>
+    <h1 className="display-4 col-md-12">What is  FoodFeed</h1>
+    <p className="aboutsub"> Food feed is taking the world of culinary arts to the next level. 
+    With this app we want to give aspiring chefs the opportunity to share their recipes and showcase their talents through a livestreaming platform.  </p>
+    <p>Sign up and showcase your talents as a Chef or sign up and witness talented people in your local town prepare some of the finest cuisine and have them delivered to your
+        front door. Share your favorite recipes and collect some new ones along the way.
+    </p>
   </div>
 </div>
   );
